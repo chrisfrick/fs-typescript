eeb4045458dc7e1bb2be469020cf1123d0dd5395
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-07-28 09:21:51 -0400

    bmi calculator Ex 9.1 body mass index

---
+++ bmi/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (heightCm: number, weightKg: number) => {
+  let bmi = weightKg / (heightCm / 100) ** 2;
+  if (bmi <= 18.5) {
+    return `BMI: ${bmi} (underweight)`;
+  } else if (bmi > 18.5 && bmi < 25) {
+    return `BMI: ${bmi} (healthy weight)`;
+  } else if (bmi >= 25 && bmi < 30) {
+    return `BMI: ${bmi} (overweight)`;
+  } else {
+    return `BMI: ${bmi} (obesity)`;
+  }
+};
+
+console.log(calculateBmi(180, 74));
