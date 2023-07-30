8537d5bb85b510c7adeb51ee86d23e3722d86471
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-07-30 15:16:45 -0400

    validating requests, type guards, 9c

--- flight_diary/src/services/diaryService.ts
+++ flight_diary/src/services/diaryService.ts
@@ -1,6 +1,10 @@
 import diaries from '../../data/entries';
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import {
+  NewDiaryEntry,
+  NonSensitiveDiaryEntry,
+  DiaryEntry,
+} from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -15,12 +19,25 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
+};
+
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
   getNonSensitiveEntries,
   addDiary,
+  findById,
 };
