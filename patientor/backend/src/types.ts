d98acfa786b6f6d519f78554f46a14e1ae9e7429
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-08-01 10:55:02 -0400

    set up parsing and validation for POST requests to /api/patients Ex 9.13 patientor backend step6

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -9,8 +9,14 @@ export interface PatientInfo {
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
+}
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
 
 export type NewPatientInfo = Omit<PatientInfo, 'id'>;
